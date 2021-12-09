7fe8ceaa0a0df4b69a597cf4c069bed6bf356995
Gabriel de La Porta <gabrieldelaporta1@gmail.com>
2021-12-09 14:16:32 -0300

    Add partial

---
+++ public/index.js
@@ -0,0 +1,200 @@
+const id = localStorage.getItem("socket");
+const socket = io(
+  "https://chat-do-gabriel.herokuapp.com/" || "http://localhost:5000",
+  {
+    query: {
+      id: id ? id : "",
+      username: localStorage.getItem("username"),
+    },
+  }
+);
+
+onInit();
+
+socket.on("logged", (data) => {
+  localStorage.setItem("socket", data.id);
+  localStorage.setItem("username", data.username);
+  onInit();
+});
+
+socket.on("receiveId", (id) => {
+  localStorage.setItem("socket", id);
+});
+
+socket.on("new-room", (room) => {
+  joinRoom(room);
+  let rooms = localStorage.getItem("rooms");
+
+  if (rooms) {
+    rooms = JSON.parse(rooms);
+    rooms.push(room);
+    localStorage.setItem("rooms", JSON.stringify(rooms));
+  } else {
+    localStorage.setItem("rooms", JSON.stringify([room]));
+  }
+  renderRoom(room);
+});
+
+socket.on("new-message", ({ roomId, message }) => {
+  let rooms = localStorage.getItem("rooms");
+  if (rooms) {
+    rooms = JSON.parse(rooms);
+    const dataRooms = rooms.map((r) => {
+      if (r.id === roomId) {
+        r.messages.push(message);
+        return r;
+      } else {
+        return r;
+      }
+    });
+    localStorage.setItem("rooms", JSON.stringify(dataRooms));
+    renderMessage({ roomId, message });
+  }
+});
+
+$("#user").submit((event) => {
+  event.preventDefault();
+  let username = $("input[name=username]").val();
+
+  if (username.length) {
+    socket.emit("login", { username: username });
+  }
+});
+
+$("#room").submit((event) => {
+  event.preventDefault();
+  let users = $("#users").val();
+  users.push(localStorage.getItem("socket"));
+  socket.emit("create-room", users);
+});
+
+$("#message").submit((event) => {
+  event.preventDefault();
+  const textValue = $("#text-message").val();
+  const roomId = $(".active-chat").attr("data-chat");
+  const rooms = localStorage.getItem("rooms");
+  const message = {
+    sender: localStorage.getItem("username"),
+    text: textValue,
+  };
+
+  if (rooms && roomId) {
+    socket.emit("send-message", { roomId: roomId, message: message });
+  } else {
+    alert("falha ao enviar mensagem");
+  }
+});
+
+/* $(".chat[data-chat=person2]").addClass("active-chat");
+$(".person[data-chat=person2]").addClass("active"); */
+
+function onInit() {
+  if (localStorage.getItem("username")) {
+    $(".container-form ").removeClass("display-block").addClass("display-none");
+    $(".container").removeClass("display-none").addClass("display-block");
+    let rooms = localStorage.getItem("rooms");
+    if (rooms) {
+      rooms = JSON.parse(rooms);
+      $(".people").empty();
+      $.each(rooms, function (i, item) {
+        renderRoom(item);
+      });
+      setEvents();
+    }
+  } else {
+    $(".container-form").removeClass("display-none").addClass("display-block");
+    $(".container").removeClass("display-block").addClass("display-none");
+  }
+}
+function setEvents() {
+  $(".person").click(function () {
+    if ($(this).hasClass(".active")) {
+      return false;
+    } else {
+      var findChat = $(this).attr("data-chat");
+      var personName = $(this).find(".name").text();
+      joinRoom({ id: findChat });
+      $(".right .top .name").html(personName);
+      $(".chat").removeClass("active-chat");
+      $(".left .person").removeClass("active");
+      $(this).addClass("active");
+      $(".chat[data-chat = " + findChat + "]").addClass("active-chat");
+      /* $(".active-chat").scrollTop($(".active-chat")[0].scrollHeight); */
+    }
+  });
+}
+
+function renderRoom(room) {
+  $(".people").append(`
+    <li class="person" id="room" data-chat="${room.id}">
+      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+          <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+          <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+      </svg>
+      <span class="name">${room.name}</span>
+      <span class="time">2:09 PM</span>
+      <span class="preview">I was wondering...</span>
+    </li>
+  `);
+  $(".right .chat-body").empty();
+  $(".right .chat-body").append(`
+    <div class="chat" data-chat="${room.id}"></div>
+  `);
+
+  setEvents();
+}
+function renderMessages(room) {
+  const username = localStorage.getItem("username");
+  $.each(room.messages, function (i, message) {
+    if (message) {
+      message.sender === username
+        ? $(".chat[data-chat = " + room.id + "]").append(`
+        <div class="bubble me">
+            ${message.text}
+        </div>
+      `)
+        : $(".chat[data-chat = " + room.id + "]").append(`
+        <div class="bubble you">
+          ${message.text}
+        </div>
+      `);
+    }
+  });
+}
+
+function renderMessage({ roomId, message }) {
+  const username = localStorage.getItem("username");
+
+  message.sender === username
+    ? $(".chat[data-chat = " + roomId + "]").append(`
+    <div class="bubble me">
+        ${message.text}
+    </div>
+  `)
+    : $(".chat[data-chat = " + roomId + "]").append(`
+    <div class="bubble you">
+      ${message.text}
+    </div>
+  `);
+}
+
+function joinRoom(room) {
+  socket.emit("join-room", room.id);
+}
+
+socket.on("active-users", (users) => {
+  $.each(users, function (i, item) {
+    if (item.username !== localStorage.getItem("username")) {
+      $("#users")
+        .find("option")
+        .remove()
+        .end()
+        .append(
+          $("<option>", {
+            value: item.id,
+            text: item.username,
+          })
+        );
+    }
+  });
+});
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
+var randomId = require("random-id");
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -15,16 +16,112 @@ app.use("/", (req, res) => {
   res.render("index.html");
 });
 
-let messages = [];
+let users = [];
+
+io.use((socket, next) => {
+  const socketId = socket.handshake.query.id;
+  const username = socket.handshake.query.username;
+  if (socketId) {
+    // find existing session
+    const user = users.find((u) => u.id === socketId);
+    if (user) {
+      users = users.map((u) => {
+        if (u.id === user.id) {
+          u.id = socket.id;
+          return u;
+        } else {
+          return u;
+        }
+      });
+      socket.username = user.username;
+
+      socket.emit("logged", { username: username, id: socket.id });
+
+      return next();
+    } else {
+      const connectUser = {
+        username: username,
+        id: socket.id,
+        rooms: [],
+      };
+      users.push(connectUser);
+      socket.emit("logged", connectUser);
+      return next();
+    }
+  }
+  next();
+});
 
 io.on("connection", (socket) => {
   console.log("Socket conectado: ", socket.id);
-
-  socket.emit("previousMessages", messages);
+  /* 
+  socket.emit("receiveId", socket.id); */
+  socket.broadcast.emit("active-users", users);
+  socket.emit("active-users", users);
 
   socket.on("sendMessage", (data) => {
     messages.push(data);
     socket.broadcast.emit("receivedMessage", data);
+  });
+
+  socket.on("login", (data) => {
+    const user = {
+      username: data.username,
+      id: socket.id,
+      rooms: [],
+    };
+    if (!users.some((u) => u.username === data.username)) {
+      users.push(user);
+      socket.emit("logged", user);
+      socket.broadcast.emit("active-users", users);
+    }
+  });
+
+  socket.on("create-room", (usersIds) => {
+    let roomName = "";
+    for (const id in usersIds) {
+      for (const u in users) {
+        if (users[u].id === usersIds[id]) {
+          roomName =
+            roomName +
+            users[u].username +
+            (id == usersIds.length - 1 ? "" : " - ");
+        }
+      }
+    }
+    const room = {
+      id: randomId(30, "aA0"),
+      name: roomName,
+      messages: [],
+    };
+    for (const id in usersIds) {
+      for (const u in users) {
+        if (users[u].id === usersIds[id]) {
+          users[u].rooms.push(room);
+          socket.to(usersIds[id]).emit("new-room", room);
+        }
+      }
+    }
+    socket.emit("new-room", room);
+  });
+
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId);
+  });
+
+  socket.on("send-message", (data) => {
+    const { roomId, message } = data;
+    for (const u in users) {
+      if (users[u].id === socket.username) {
+        users[u].rooms;
+        for (const r in users[u].rooms) {
+          if (users[u].rooms[r].id === roomId) {
+            users[u].rooms[r].messages.push(message);
+          }
+        }
+      }
+    }
+    io.to(roomId).emit("new-message", data);
   });
 });
 
